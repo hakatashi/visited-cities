0570dfa8109446fec308bc91d5bbf43d88a9e93b
Koki Takahashi <hakatasiloving@gmail.com>
2023-02-14 13:19:43 +0000

    Add cities scrape script

---
+++ bin/scrape.ts
@@ -0,0 +1,24 @@
+import download from 'download';
+import fs from 'fs-extra';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const prefs = ['北海道', '青森県', '岩手県', '宮城県', '秋田県', '山形県', '福島県', '茨城県', '栃木県', '群馬県', '埼玉県', '千葉県', '東京都', '神奈川県', '新潟県', '富山県', '石川県', '福井県', '山梨県', '長野県', '岐阜県', '静岡県', '愛知県', '三重県', '滋賀県', '京都府', '大阪府', '兵庫県', '奈良県', '和歌山県', '鳥取県', '島根県', '岡山県', '広島県', '山口県', '徳島県', '香川県', '愛媛県', '高知県', '福岡県', '佐賀県', '長崎県', '熊本県', '大分県', '宮崎県', '鹿児島県', '沖縄県'];
+const cities = [];
+
+(async () => {
+  for (const [i, pref] of prefs.entries()) {
+    const res = await download(`https://www.land.mlit.go.jp/webland/api/CitySearch?area=${(i + 1).toString().padStart(2, '0')}`);
+    const data = JSON.parse(res.toString());
+    cities.push(...(data.data as {id: string, name: string}[]).map((city) => ({
+      id: city.id,
+      name: city.name,
+      pref,
+    })));
+  }
+  await fs.ensureDir(`${__dirname}/../src/data`);
+  await fs.writeJson(`${__dirname}/../src/data/cities.json`, cities);
+})();
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,20 @@
 import { EventIterator } from "event-iterator"
 import JSZip from "jszip";
+import cities from '../data/cities';
+import type { JSX } from "solid-js";
+import dayjs from "dayjs";
+
+const citiesRegex = new RegExp(`(?<city>${cities.map((city) => city.pref + city.name).join('|')})`, '')
 
 export default function Home() {
 	const reader = new FileReader();
 
-	const handleImageChange = (event) => {
-		const [file] = event.target.files;
-		reader.onload = loadZip;
-		reader.readAsArrayBuffer(file);
+	const handleImageChange: JSX.EventHandler<HTMLInputElement, Event> = (event) => {
+		const file = event.currentTarget.files?.[0];
+		if (file) {
+			reader.onload = loadZip;
+			reader.readAsArrayBuffer(file);
+		}
 	}
 
 	const loadZip = async () => {
@@ -16,13 +23,17 @@ export default function Home() {
 		}
 
 		const zip = await JSZip.loadAsync(reader.result);
-		const iterator = new EventIterator<JSZip.JSZipObject>(({ push }) => {
+		const iterator = new EventIterator<JSZip.JSZipObject>(({push, stop}) => {
 			zip.forEach((relativePath, file) => {
 				if (relativePath.includes('Semantic Location History') && relativePath.endsWith('.json')) {
 					push(file);
 				}
 			});
+			stop();	
 		});
+
+		const citiesSet = new Set<string>();
+		const citiesHistory: {city: string, date: string}[] = [];
 
 		for await (const file of iterator) {
 			const data = await file.async('string');
@@ -31,10 +42,23 @@ export default function Home() {
 
 			for (const event of events) {
 				if ({}.hasOwnProperty.call(event, 'placeVisit')) {
-					console.log(event.placeVisit?.location?.address);
+					const address: string = event.placeVisit?.location?.address ?? '';
+					const match = citiesRegex.exec(address);
+					if (match) {
+						const city: string = match?.groups?.city ?? '';
+						if (!citiesSet.has(city)) {
+							citiesSet.add(city);
+							citiesHistory.push({
+								city,
+								date: dayjs(event?.placeVisit?.duration?.startTimestamp ?? '').format('YYYY/MM/DD'),
+							});
+						}
+					}
 				}
 			}
 		}
+
+		console.log(citiesHistory);
 	}
 
 	return (
